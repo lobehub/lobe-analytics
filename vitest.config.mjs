f5e131a972723ee8981a22dea0660442fe607553
Tsuki <976499226@qq.com>
2025-06-30 16:22:04 +0800

    🐛 fix: Vitest issue

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,14 +1,17 @@
 import dotenv from 'dotenv';
-import path from 'node:path';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vitest/config';
 
 // Load environment variables from .env file
 dotenv.config();
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': resolve(__dirname, './src'),
     },
   },
   test: {
